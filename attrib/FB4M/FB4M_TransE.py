81e717596bfacb69b6c0d57dd640b4cf9b7d54c3
ishijiaxin <ishijiaxin@126.com>
2015-08-05 15:39:46 +0800

    For running

--- attrib/FB4M/FB4M_TransE.py
+++ attrib/FB4M/FB4M_TransE.py
@@ -1,6 +1,22 @@
 #! /usr/bin/python
 from FB4M_exp import *
 
-launch(op='TransE', simfn='L1', ndim=50, nhid=50, marge=1., lrweights=1e-6, momentum=0.9, lremb=0.01, lrparam=1.,
-    nbatches=4000, totepochs=1000, test_all=10, neval=1000, savepath='FB4M_TransE', datapath='../data/', eval_batchsize=40000)
-
+launch(
+    op='TransE',
+    simfn='L1',
+    ndim=50,
+    nhid=50,
+    margin=1.,
+    lrweights=1e-6,
+    momentum=0.9,
+    lremb=0.01,
+    lrparam=1.,
+    nbatches=4000,
+    totepochs=1000,
+    test_all=10,
+    neval=1000,
+    savepath='FB4M_TransE',
+    datapath='../data/',
+    eval_batchsize=40000,
+    entity_batchsize=40000
+)
